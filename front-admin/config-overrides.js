d06a7411a8e4d119fbb855b271a8d1fd64de73f3
jracine <julien.racine@pole-emploi.fr>
2021-01-06 14:34:01 +0100

    test CI

--- front-admin/config-overrides.js
+++ front-admin/config-overrides.js
@@ -1,12 +1,3 @@
 // override create-react-app config
 
 /* eslint-disable no-param-reassign */
-module.exports = function override(config, env) {
-  // manage root path
-  const isEnvDevelopment = env === 'development'
-  if (isEnvDevelopment) {
-    config.output.publicPath = '/admin'
-  }
-
-  return config
-}
