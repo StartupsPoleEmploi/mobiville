2792f5ea5d4cbde06ee350e94480611a7e6c10f2
François-Xavier Montigny <fxaviermontigny@gmail.com>
2021-01-20 12:58:57 +0100

    add tag commander

--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,16 @@ import React from 'react'
 import { Routes } from './routes/routes'
 
 function App() {
+  // import tag commander
+  const script = document.createElement('script')
+  if (process.env.REACT_APP_ZEN_ENV !== 'production') {
+    script.src = 'http://cdn.tagcommander.com/5595/tc_Mobiville_31.js'
+  } else {
+    script.src = 'http://cdn.tagcommander.com/5595/uat/tc_Mobiville_31.js'
+  }
+  script.async = true
+  document.body.appendChild(script)
+
   return (<Routes />)
 }
 
