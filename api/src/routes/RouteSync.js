76f2dd712c1b818d1c8ba83fdd03d03ac02e9ee3
François-Xavier Montigny <fxaviermontigny@gmail.com>
2021-03-04 18:20:31 +0100

    update sync method

--- api/src/routes/RouteSync.js
+++ api/src/routes/RouteSync.js
@@ -4,20 +4,6 @@ import Route from './Route'
 export default class RouteSync extends Route {
   constructor(params) {
     super({ ...params, model: 'cities' })
-
-    try {
-      this.autoSync()
-    } catch(err) {
-      console.log(err)
-    }
-  }
-
-  async autoSync() {
-    const countAmenities = await this.models.amenities.count()
-    if(countAmenities === 0) {
-      const amenities = await getAmenitiesDatas()
-      await this.models.amenities.sync({amenities})
-    }
   }
 
   @Route.Get()
@@ -45,5 +31,13 @@ export default class RouteSync extends Route {
     const statusTensions = await this.models.tensions.syncTensions({tensions})
 
     this.sendOk(ctx, {tensions: statusTensions, bassins: statusBassins})
+  }
+
+  @Route.Get()
+  async syncAmenities(ctx) {
+    const amenities = await getAmenitiesDatas()
+    const statusAmenities = await this.models.amenities.sync({amenities})
+
+    this.sendOk(ctx, {amenities: statusAmenities})
   }
 }
