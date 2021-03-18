13f6020d696c676d43bba71d7916d9512c23ea2c
François-Xavier Montigny <fxaviermontigny@gmail.com>
2021-03-18 15:47:04 +0100

    add criterions fiter

--- front/src/routes/cities/desktop-criterions-panel.js
+++ front/src/routes/cities/desktop-criterions-panel.js
@@ -1,4 +1,4 @@
-import React, { /* useEffect, */ useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import {
@@ -113,15 +113,18 @@ const DesktopCriterionsPanel = ({ criterions, total }) => {
     setOnSearch(params)
   }
 
-  /* useEffect(() => {
-    setTempForm({
-      ...tempForm,
-      rome: criterions && criterions.code_rome
-      && criterions.code_rome.length ? criterions.code_rome[0] : '',
-      region: criterions && criterions.code_region
-      && criterions.code_region.length ? criterions.code_region[0] : ''
-    })
-  }, [criterions]) */
+  useEffect(() => {
+    const rome = criterions && criterions.code_rome && criterions.code_rome.length ? criterions.code_rome[0] : ''
+    const region = criterions && criterions.code_region && criterions.code_region.length ? criterions.code_region[0] : ''
+
+    if (criterions && (rome !== tempForm.rome || region !== tempForm.region)) {
+      setTempForm({
+        ...tempForm,
+        rome,
+        region
+      })
+    }
+  }, [criterions])
 
   if (allCriterions.criterions && criterions && criterions.code_criterion) {
     if (tempForm.environment == null && criterions) {
