123457871187d5738ff568875111ceef86445925
François-Xavier Montigny <fxaviermontigny@gmail.com>
2021-03-22 12:49:20 +0100

    fix tag co

--- front/src/App.js
+++ front/src/App.js
@@ -4,7 +4,7 @@ import { Routes } from './routes/routes'
 function App() {
   // import tag commander
   const script = document.createElement('script')
-  if (process.env.NODE_ENV === 'production') {
+  if (window.location.host === 'mobiville.pole-emploi.fr') {
     script.src = 'https://cdn.tagcommander.com/5595/tc_Mobiville_31.js'
   } else {
     script.src = 'https://cdn.tagcommander.com/5595/uat/tc_Mobiville_31.js'
