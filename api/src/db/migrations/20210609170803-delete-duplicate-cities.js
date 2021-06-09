ac438b7400797946938790f707a0e327b19b9c13
Hugo Agbonon <hugo@codeheroics.com>
2021-06-09 19:11:38 +0200

    temp fix - remove duplicated cities from qa (before adding unique key)

---
+++ api/src/db/migrations/20210609170803-delete-duplicate-cities.js
@@ -0,0 +1,26 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.sequelize.query(`delete
+      from cities using cities,
+        cities c1
+      where cities.id > c1.id
+        and cities.insee_com = c1.insee_com`)
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+}
