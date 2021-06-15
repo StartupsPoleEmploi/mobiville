16f7b933ff4b9baff88443c97ceef7ae8c09d9e6
Hugo Agbonon <hugo@codeheroics.com>
2021-06-15 16:10:03 +0200

    add console scripts for sync of regions, cities, professions in tension

---
+++ api/src/scripts/syncRegions.js
@@ -0,0 +1,22 @@
+// A script to execute all syncs that were previously done in the sync route
+import { getAllRegions } from '../utils/api'
+
+import db from '../models'
+
+const models = db.initModels()
+
+const doSync = async () => {
+  try {
+    const regions = await getAllRegions()
+    const status = await models.regions.syncRegions({regions})
+    console.log('Success! Status:', status)
+    process.exit(0)
+  } catch (err) {
+    console.error(err)
+    console.error('Error! Sync failed and will now exit')
+    process.exit(1)
+  }
+}
+
+console.log('Starting synchronization for cities. This should be almost instanteneous. Please wait for the confirmation message…')
+doSync()
