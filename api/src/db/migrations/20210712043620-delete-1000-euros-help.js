bd7070a3f918c81dd5fcc189149d4d21eca38e5c
Hugo Agbonon <hugo@codeheroics.com>
2021-07-12 06:54:18 +0200

    migration to remove a specific help from db

---
+++ api/src/db/migrations/20210712043620-delete-1000-euros-help.js
@@ -0,0 +1,16 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query('delete from helps where id=17;')
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+}
