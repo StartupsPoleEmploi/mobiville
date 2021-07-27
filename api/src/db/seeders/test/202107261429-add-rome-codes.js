9c5deda3198fa3a21632cf88bba783b8f591bdd5
Hugo Agbonon <hugo@codeheroics.com>
2021-07-27 15:51:36 +0200

    add missing data for rome codes

---
+++ api/src/db/seeders/test/202107261429-add-rome-codes.js
@@ -0,0 +1,23 @@
+const { readFileSync } = require( 'fs')
+const parser = require('xml2json')
+
+module.exports = {
+  up: async (queryInterface, Sequelize, models) => {
+    const data = JSON.parse(
+      parser.toJson(readFileSync(__dirname + '/../../../assets/datas/unix_referentiel_code_rome_v346_utf8.xml'))
+    ).ogr.item_referentiel_code_rome
+
+    await models.romeCodes.destroy({
+      where: {},
+      truncate: true,
+    })
+
+    for (const row of data) {
+      await models.romeCodes.create({
+        code: row.code_rome,
+        label: row.libelle,
+      })
+    }
+
+  },
+}
