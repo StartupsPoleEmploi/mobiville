ae0d186338be33302f649c83fa9310312decc8c7
Hugo Agbonon <hugo@codeheroics.com>
2021-07-28 11:53:54 +0200

    ogr list is now a rome list

--- front/src/routes/ogr-list/OgrList.js
+++ front/src/routes/ogr-list/OgrList.js
@@ -2,15 +2,85 @@ import React, { useState, useEffect } from 'react'
 import { Link as _Link } from 'react-router-dom'
 import styled from 'styled-components'
 import axios from 'axios'
-import { sortBy } from 'lodash'
+import { deburr, sortBy } from 'lodash'
 import ArrowBackOutlinedIcon from '@material-ui/icons/ArrowBackOutlined'
 
 import { MainLayout } from '../../components/main-layout'
 import { isMobileView } from '../../constants/mobile'
 import { useWindowSize } from '../../common/hooks/window-size'
 
+import aSVG from '../../assets/images/rome/A.svg'
+import bSVG from '../../assets/images/rome/B.svg'
+import cSVG from '../../assets/images/rome/C.svg'
+import dSVG from '../../assets/images/rome/D.svg'
+import fSVG from '../../assets/images/rome/F.svg'
+import gSVG from '../../assets/images/rome/G.svg'
+import hSVG from '../../assets/images/rome/H.svg'
+import iSVG from '../../assets/images/rome/I.svg'
+import jSVG from '../../assets/images/rome/J.svg'
+import kSVG from '../../assets/images/rome/K.svg'
+import mSVG from '../../assets/images/rome/M.svg'
+import nSVG from '../../assets/images/rome/N.svg'
+
 const PAGE_HEADER_SIZE = 92
 const MAIN_HEADER_SIZE = 0
+
+const MAIN_CATEGORIES = {
+  A: {
+    label: 'Agriculture et Pêche, Espaces naturels et Espaces verts, Soins aux animaux',
+    svg: aSVG
+  },
+  B: {
+    label: "Arts et Façonnage d'ouvrages d'art",
+    svg: bSVG
+  },
+  C: {
+    label: 'Banque, Assurance, Immobilier',
+    svg: cSVG
+  },
+  D: {
+    label: 'Commerce, Vente et Grande distribution',
+    svg: dSVG
+  },
+  E: {
+    label: 'Communication, Média et Multimédia'
+  },
+  F: {
+    label: 'Construction, Bâtiment et Travaux publics',
+    svg: fSVG
+  },
+  G: {
+    label: 'Hôtellerie-Restauration, Tourisme, Loisirs et Animation',
+    svg: gSVG
+  },
+  H: {
+    label: 'Industrie',
+    svg: hSVG
+  },
+  I: {
+    label: 'Installation et Maintenance',
+    svg: iSVG
+  },
+  J: {
+    label: 'Santé',
+    svg: jSVG
+  },
+  L: {
+    label: 'Spectacle'
+  },
+  K: {
+    label: 'Services à la personne et à la collectivité',
+    svg: kSVG
+  },
+  M: {
+    label: "Support à l'entreprise",
+    svg: mSVG
+  },
+  N: {
+    label: 'Transport et Logistique',
+    svg: nSVG
+  }
+}
 
 const Wrapper = styled.div`
   margin: auto;
@@ -44,6 +114,14 @@ const H1 = styled.h1`
 }
 `
 
+const CategoryDiv = styled.div`
+  background: #FFFFFF;
+  box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.14), 0px 2px 2px rgba(0, 0, 0, 0.12), 0px 1px 3px rgba(0, 0, 0, 0.2);
+  border-radius: 16px;
+  padding: 16px;
+  margin-bottom: 16px;
+`
+
 const H2 = styled.h2`
   && {
     font-size: 18px;
@@ -70,12 +148,14 @@ const OgrList = () => {
   const isMobile = isMobileView(size)
 
   const [isLoading, setIsLoading] = useState(true)
-  const [ogrsByRome, setOgrsByRome] = useState({})
+  const [dataByCategory, setDataByCategory] = useState({})
 
   useEffect(() => {
     setIsLoading(true)
 
     axios.get('/api/ogrs').then((response) => {
+      response.data.data.sort((a, b) => (deburr(a.romeLabel) < deburr(b.romeLabel) ? 1 : -1))
+
       const groupedData = sortBy(
         response.data.data,
         ['romeLabel', 'ogrLabel']
@@ -83,12 +163,25 @@ const OgrList = () => {
         .reduce((prev, { rome, romeLabel, ogrLabel }) => ({
           ...prev,
           [rome]: {
+            rome,
             label: romeLabel,
             ogrs: (prev[rome]?.ogrs || []).concat(ogrLabel)
           }
         }), {})
 
-      setOgrsByRome(groupedData)
+      const keys = Object.keys(groupedData)
+      keys.sort((a, b) => (deburr(a) > deburr(b) ? 1 : -1))
+
+      const dataGroupedByRomeCategory = keys.reduce((prev, rome) => {
+        const romeFirstChar = rome[0]
+
+        return {
+          ...prev,
+          [romeFirstChar]: (prev[romeFirstChar] || []).concat(groupedData[rome])
+        }
+      }, {})
+
+      setDataByCategory(dataGroupedByRomeCategory)
       setIsLoading(false)
     })
   }, [])
@@ -115,17 +208,23 @@ const OgrList = () => {
           Les métiers disponibles sur Mobiville
         </H1>
         )}
-        {Object.keys(ogrsByRome).map((romeCode) => (
-          <div>
-            <H2>{ogrsByRome[romeCode].label}</H2>
+        {Object.keys(dataByCategory).map((categoryLetter) => (
+          <CategoryDiv key={categoryLetter}>
+            {MAIN_CATEGORIES[categoryLetter].svg && <img src={MAIN_CATEGORIES[categoryLetter].svg} alt="" />}
+            <H2>{MAIN_CATEGORIES[categoryLetter].label}</H2>
             <Ul>
-              {ogrsByRome[romeCode].ogrs.map((label) => (
-                <Li>
-                  <Link to={`/rechercher/region?code_rome=${romeCode}`}>{label}</Link>
+              {Object.keys(dataByCategory[categoryLetter]).map((index) => (
+                <Li key={dataByCategory[categoryLetter][index].rome}>
+                  <Link
+                    to={`/rechercher/region?code_rome=${dataByCategory[categoryLetter][index].rome}`}
+                    title={dataByCategory[categoryLetter][index].ogrs.join(', ')}
+                  >
+                    {dataByCategory[categoryLetter][index].label}
+                  </Link>
                 </Li>
               ))}
             </Ul>
-          </div>
+          </CategoryDiv>
         ))}
       </Wrapper>
     </MainLayout>
