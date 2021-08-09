ef82265578b90fb0567f7645e9bf9af23539808f
Hugo Agbonon <hugo@codeheroics.com>
2021-08-09 17:46:17 +0200

    handle errors in search

---
+++ front/src/routes/error/ErrorPage.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from 'styled-components'
+import { MainLayout } from '../../components/main-layout'
+
+import techErrorPic from '../../assets/images/technical-error.svg'
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  padding-top: 5rem;
+  font-size: 1.5rem;
+  line-height: 1.5;
+  color: #657078;
+`
+
+const ErrorPage = () => (
+  <MainLayout>
+    <Container>
+      <img
+        alt=""
+        src={techErrorPic}
+        style={{ marginBottom: '2rem' }}
+      />
+      Problème technique
+      <br />
+      Veuillez réessayer plus tard
+    </Container>
+  </MainLayout>
+)
+
+export default React.memo(ErrorPage)
