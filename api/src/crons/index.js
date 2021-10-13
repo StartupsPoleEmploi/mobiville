e920770609d2ad66ba200cddf531eaba465c4b2b
Hugo Agbonon <hugo@codeheroics.com>
2021-10-13 12:50:57 +0200

    api: add regions_tensions_criterions table, use it instead of complicated query

--- api/src/crons/index.js
+++ api/src/crons/index.js
@@ -1,9 +1,7 @@
 import cities from './cron-cities'
-import regionTensionsCron from './cron-regionsTensions'
 import bassinsJobsCron from './cron-bassinsJobs'
 
 export const start = (env) => {
   cities(env)
-  regionTensionsCron(env)
   bassinsJobsCron(env)
 }
