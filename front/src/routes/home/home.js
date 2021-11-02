0086b78ec4241372589bfb5a2ac8a718b31b6bef
Hugo Agbonon <hugo@codeheroics.com>
2021-11-02 12:11:46 +0100

    Write new home page (desktop version)

--- front/src/routes/home/home.js
+++ front/src/routes/home/home.js
@@ -4,200 +4,138 @@ import { Link } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
 
 import { MainLayout } from '../../components/main-layout'
-import {
-  COLOR_TEXT_PRIMARY,
-  COLOR_PRIMARY,
-  COLOR_GRAY,
-} from '../../constants/colors'
-import STEP1 from '../../assets/images/01-Selectionnez.png'
-import STEP2 from '../../assets/images/02-Ajoutez.png'
-import STEP3 from '../../assets/images/03-Decouvrez.png'
-import STEP4 from '../../assets/images/04-Identifiez.png'
-import STEP5 from '../../assets/images/05-Demenagez.png'
+import { COLOR_TEXT_PRIMARY, COLOR_PRIMARY } from '../../constants/colors'
 import { useWindowSize } from '../../common/hooks/window-size'
 import { isMobileView } from '../../constants/mobile'
 
-const Block = styled.div`
-  padding: 64px 0;
+import heroHomepagePic from '../../assets/images/00-Hero-Homepage.png'
+import { ReactComponent as MagnifyingGlassIcon } from '../../assets/images/magnifying-glass.svg'
+import { ReactComponent as SuperheroIcon } from '../../assets/images/superhero.svg'
+import { ReactComponent as MapPointerIcon } from '../../assets/images/map-pointer.svg'
+import { ReactComponent as PrizeIcon } from '../../assets/images/prize.svg'
+import { ReactComponent as MoneyClockIcon } from '../../assets/images/money-clock.svg'
 
-  > .wrapper {
-    display: flex;
-    max-width: 1040px;
-    align-items: center;
-
-    > div:first-child {
-      margin-right: 64px;
-    }
-  }
-
-  ${(props) =>
-    props.isMobile
-      ? `
-    > .wrapper {
-      flex-direction: column;
-      margin-left: 16px;
-      margin-right: 16px;
-      width: calc(100% - 32px);
-
-      > div:first-child {
-        margin-right: 0;
-      }
-    }
-  `
-      : ''}
-`
-
-const ImageBlock = styled.div`
-  width: 320px;
-  height: 320px;
-  background-size: contain;
-  background-position: center;
-  background-repeat: no-repeat;
-
-  ${(props) =>
-    props.isMobile
-      ? `
-    width: 256px;
-    height: 256px;
-    margin-bottom: 16px;
-  `
-      : ''}
-`
-
-const VideoBlock = styled.video`
-  width: 320px;
-  height: 320px;
-`
-
-const TextBlock = styled.div`
-  flex: 1;
-`
-
-const Title = styled.h1`
+const H1 = styled.h1`
   font-size: 36px;
-  font-weight: bold;
-  color: ${COLOR_TEXT_PRIMARY};
-
-  ${(props) =>
-    props.isMobile
-      ? `
-    font-size: 18px;
-
-  `
-      : ''}
 `
 
-const Description = styled.h3`
-  margin: 16px 0 32px 0;
-  font-size: 24px;
-  font-weight: bold;
-  color: ${COLOR_TEXT_PRIMARY};
-
-  ${(props) =>
-    props.isMobile
-      ? `
-    font-size: 14px;
-  `
-      : ''}
+const TopBlockContainer = styled.div`
+  background: linear-gradient(180deg, #ddddea 0%, #c3e9e9 100%);
 `
 
-const Action = styled(Link)`
-  && {
-    text-decoration: none;
-    color: white;
-    background-color: ${COLOR_PRIMARY};
-    height: 48px;
-    line-height: 48px;
-    font-size: 18px;
-    cursor: pointer;
-    border-radius: 48px;
-    display: inline-block;
-    width: 311px;
-    text-align: center;
+const BlocksContainer = styled.div`
+  background-color: #fff;
+  padding-top: 64px;
+`
+
+const Block = styled.div`
+  width: 100%;
+  max-width: 1040px;
+  margin: auto;
+  padding: 8px 16px;
+`
+
+const AdvantageBlocksContainer = styled.div`
+  display: flex;
+  margin: 0 auto 32px;
+`
+
+const AdvantageBlock = styled.div`
+  background: #f6f7fb;
+  border-radius: 8px;
+  display: flex;
+  max-width: 336px;
+  width: 100%;
+  overflow: hidden;
+  padding: 16px;
+
+  &:not(:first-child) {
+    margin-left: 16px;
   }
 `
 
-const How = styled.div`
-  margin: 64px auto;
-  max-width: 688px;
+const AdvantageIconContainer = styled.div`
+  flex-shrink: 0;
+  margin-right: 16px;
+`
+
+const AdvantageTextH3 = styled.h3`
+  margin-top: 0;
+`
+
+const AdvantageTextP = styled.p`
+  margin: 0;
+`
+
+const MainNavButtons = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #fff;
+  border: 1px solid ${COLOR_PRIMARY};
+  box-shadow: 6px 6px 0px #37b4b4;
+  border-radius: 16px;
+  color: ${COLOR_PRIMARY};
+  font-weight: 500;
+  font-size: 18px;
+  padding: 16px;
+  max-width: 424px;
   width: 100%;
 
-  ${(props) =>
-    props.isMobile
-      ? `
-    margin: 32px auto;
-
-    > h1 {
-      margin-bottom: 32px;
-    }
-  `
-      : ''}
-`
-
-const BlockLine = styled.div`
-  display: flex;
-
-  > div:first-child {
-    margin-right: 8px;
-  }
-
-  > div:last-child {
-    margin-left: 8px;
-  }
-
-  ${(props) =>
-    props.isMobile
-      ? `
-    margin-bottom: 32px;
-    margin-left: 16px;
+  &:first-of-type {
     margin-right: 16px;
-    width: calc(100% - 32px);
-
-
-    > div:first-child {
-      margin-right: 0;
-    }
-
-    > div:last-child {
-      margin-left: 0;
-    }
-  `
-      : ''}
+  }
 `
 
-const Panel = styled.div`
-  flex: 1;
-  text-align: center;
-`
-
-const NumberText = styled.p`
-  text-align: center;
-  margin: 0;
+const actionButtonStyle = `
+  text-decoration: none;
+  background-color: ${COLOR_PRIMARY};
+  height: 48px;
   font-size: 18px;
-  font-weight: 500;
+  cursor: pointer;
+  border-radius: 48px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  max-width: 205px;
 `
 
-const DescriptionText = styled.p`
+const ActionButton = styled(Link)`
+  &,&: hover {
+    color: #fff;
+  }
+
+  ${actionButtonStyle}
+`
+
+const ActionButtonSecondary = styled.a`
+  ${actionButtonStyle}
+  background-color: #fff;
+  color: ${COLOR_PRIMARY};
+  margin: auto;
+`
+
+const MobilityGuideBlock = styled(Block)`
+  padding: 0 64px;
+  background: #c3e9e9;
+  border-radius: 16px;
+  margin-bottom: 32px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const SurveyBlock = styled(Block)`
+  padding: 32px;
   text-align: center;
-  margin: 0;
-  font-size: 18px;
-  font-weight: 500;
-`
-
-const EmptyPanel = styled.div`
-  flex: 1;
-
-  ${(props) =>
-    props.isMobile
-      ? `
-    display: none;
-  `
-      : ''}
+  color: white;
+  background: #191970;
+  border-radius: 16px;
+  margin-bottom: 32px;
 `
 
 const HomePage = () => {
-  const size = useWindowSize()
-
   return (
     <MainLayout footer topMobileMenu>
       <Helmet>
@@ -207,111 +145,103 @@ const HomePage = () => {
           content="Mobiville est un service vous permettant de trouver la ville qui correspond à votre besoin ainsi que les aides financières à la mobilité."
         />
       </Helmet>
-      <Block isMobile={isMobileView(size)}>
-        <div className="wrapper">
-          {isMobileView(size) && (
-            <ImageBlock
-              style={{ backgroundImage: 'url(/00-Hero-Homepage.png)' }}
-              isMobile={isMobileView(size)}
-            />
-          )}
-          <TextBlock>
-            <Title isMobile={isMobileView(size)}>
-              Vous souhaitez bouger pour retrouver un emploi ?
-            </Title>
-            <Description isMobile={isMobileView(size)}>
-              Trouver le job dans la ville de vos rêves
-            </Description>
-            <Action to="/rechercher">Trouver ma ville</Action>
-          </TextBlock>
-          {!isMobileView(size) && (
-            <VideoBlock loop muted autoPlay tabIndex="-1">
-              <source src="/videos/Hero-mobiville-320px.mp4" type="video/mp4" />
-            </VideoBlock>
-          )}
-        </div>
-      </Block>
 
-      <Block
-        style={{ backgroundColor: COLOR_GRAY }}
-        isMobile={isMobileView(size)}
-      >
-        <div className="wrapper">
-          <ImageBlock
-            style={{ backgroundImage: 'url(/Generique_Aides.png)' }}
-            isMobile={isMobileView(size)}
-          />
-          <TextBlock>
-            <Title isMobile={isMobileView(size)}>
-              Vous avez besoin d{"'"}
-              aide pour votre projet de mobilité ?
-            </Title>
-            <Description isMobile={isMobileView(size)}>
-              Découvrez les solutions pour accélérer votre projet
-            </Description>
-            <Action to="/aides">Découvrir les aides</Action>
-          </TextBlock>
-        </div>
-      </Block>
+      <TopBlockContainer>
+        <Block style={{ textAlign: 'center', padding: 64 }}>
+          <H1>Trouver l’emploi, et la ville qui va avec !</H1>
+          <div
+            style={{ display: 'flex', justifyContent: 'center', padding: 32 }}
+          >
+            <MainNavButtons to="/rechercher">
+              <MagnifyingGlassIcon />
+              <span>Rechercher ou découvrir une ville</span>
+            </MainNavButtons>
+            <MainNavButtons to="/helps">
+              <SuperheroIcon />
+              <span>Les aides pour mon projet</span>
+            </MainNavButtons>
+          </div>
+          <p style={{ fontWeight: 500, fontSize: 18, margin: 0 }}>
+            Mobiville vous aide à identifier les territoires dans lesquels il y
+            a de l’emploi et peu de concurrence afin d’accélérer votre retour à
+            l’emploi
+          </p>
+        </Block>
+      </TopBlockContainer>
 
-      <How isMobile={isMobileView(size)}>
-        <Title style={{ textAlign: 'center' }} isMobile={isMobileView(size)}>
-          Comment ça marche ?
-        </Title>
+      <BlocksContainer>
+        <Block style={{ padding: 0 }}>
+          <h2 style={{ margin: '0 auto 32px', textAlign: 'center' }}>
+            Les avantages de votre service Mobiville
+          </h2>
+          <AdvantageBlocksContainer>
+            <AdvantageBlock>
+              <AdvantageIconContainer>
+                <MapPointerIcon />
+              </AdvantageIconContainer>
+              <div>
+                <AdvantageTextH3>
+                  Identifiez la meilleure destination
+                </AdvantageTextH3>
+                <AdvantageTextP>
+                  Mobiville vous donne une visibilité sur le marché de l’emploi,
+                  de l’immobilier et le cadre de vie afin de choisir une ville
+                  qui correspond à votre profil.
+                </AdvantageTextP>
+              </div>
+            </AdvantageBlock>
+            <AdvantageBlock>
+              <AdvantageIconContainer>
+                <PrizeIcon />
+              </AdvantageIconContainer>
+              <div>
+                <AdvantageTextH3>Évitez la concurrence</AdvantageTextH3>
+                <AdvantageTextP>
+                  Notre service vous propose en priorité les villes avec peu de
+                  candidats sur le territoire afin d’optimiser votre recherche
+                  d’emploi.
+                </AdvantageTextP>
+              </div>
+            </AdvantageBlock>
+            <AdvantageBlock>
+              <AdvantageIconContainer>
+                <MoneyClockIcon />
+              </AdvantageIconContainer>
+              <div>
+                <AdvantageTextH3>
+                  Gagnez du temps et de l’argent
+                </AdvantageTextH3>
+                <AdvantageTextP>
+                  Découvrez des conseils et des aides financières,
+                  administratives ou humaines que vous pouvez mobiliser dans
+                  votre projet.
+                </AdvantageTextP>
+              </div>
+            </AdvantageBlock>
+          </AdvantageBlocksContainer>
+        </Block>
 
-        <BlockLine isMobile={isMobileView(size)}>
-          <Panel>
-            <img src={STEP1} alt="Sélectionnez" />
-            <NumberText>1</NumberText>
-            <DescriptionText>
-              Sélectionnez votre métier parmi la liste proposée
-            </DescriptionText>
-          </Panel>
-          <EmptyPanel isMobile={isMobileView(size)} />
-        </BlockLine>
+        <MobilityGuideBlock>
+          <div>
+            <h2>Par où commencer ?... Où aller ?... Comment faire ?...</h2>
+            <h3>Les réponses se trouvent dans notre guide à la mobilité</h3>
+            <ActionButton to="/mobility-guide">Accéder au guide</ActionButton>
+          </div>
+          <img src={heroHomepagePic} alt="" />
+        </MobilityGuideBlock>
 
-        <BlockLine isMobile={isMobileView(size)}>
-          <EmptyPanel isMobile={isMobileView(size)} />
-          <Panel>
-            <img src={STEP2} alt="Ajoutez" />
-            <NumberText>2</NumberText>
-            <DescriptionText>
-              Ajoutez vos critères professionnels et votre cadre de vie idéal
-            </DescriptionText>
-          </Panel>
-        </BlockLine>
-
-        <BlockLine isMobile={isMobileView(size)}>
-          <Panel>
-            <img src={STEP3} alt="Découvrez" />
-            <NumberText>3</NumberText>
-            <DescriptionText>
-              Découvrez des villes qui vous correspondent et qui recrutent
-            </DescriptionText>
-          </Panel>
-          <EmptyPanel isMobile={isMobileView(size)} />
-        </BlockLine>
-
-        <BlockLine isMobile={isMobileView(size)}>
-          <EmptyPanel isMobile={isMobileView(size)} />
-          <Panel>
-            <img src={STEP4} alt="Identifiez" />
-            <NumberText>4</NumberText>
-            <DescriptionText>
-              Identifiez les aides qui vont faciliter votre projet
-            </DescriptionText>
-          </Panel>
-        </BlockLine>
-
-        <BlockLine isMobile={isMobileView(size)}>
-          <Panel>
-            <img src={STEP5} alt="Déménagez" />
-            <NumberText>5</NumberText>
-            <DescriptionText>Déménagez dans votre future ville</DescriptionText>
-          </Panel>
-          <EmptyPanel isMobile={isMobileView(size)} />
-        </BlockLine>
-      </How>
+        <SurveyBlock>
+          <h2>Mobiville a besoin de votre aide pour s’améliorer</h2>
+          <h3>Aidez nous en répondant à notre enquête de satisfaction</h3>
+          <ActionButtonSecondary
+            href="https://startupsbeta.typeform.com/to/kPDt1Rfk"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Répondre à l’enquête
+          </ActionButtonSecondary>
+        </SurveyBlock>
+      </BlocksContainer>
     </MainLayout>
   )
 }
