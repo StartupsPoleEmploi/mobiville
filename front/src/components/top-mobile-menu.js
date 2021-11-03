62b6aa8de1ef5614d7494eb908ff9019a86e853a
Hugo Agbonon <hugo@codeheroics.com>
2021-11-03 16:03:05 +0100

    front: new home page for mobile

--- front/src/components/top-mobile-menu.js
+++ front/src/components/top-mobile-menu.js
@@ -1,55 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-import { Typography } from '@mui/material'
 import LOGO from '../assets/images/LogoMobiville_gros.svg'
-import LOGO_AL from '../assets/images/logo-action-logement.png'
-import LOGO_PE from '../assets/images/logo-pole-emploi.png'
-
-const Text = styled(Typography)`
-  && {
-    font-weight: 500;
-    font-size: 14px;
-    color: inherit;
-    margin-left: 10px;
-  }
-`
-
-const LogoImage = styled.img`
-  height: 40px;
-`
-
-const LogoImagePartener = styled.img`
-  height: 28px;
-  margin-left: 16px;
-`
+import LOGO_FR from '../assets/images/marianne-logo.png'
 
 const Header = styled.header`
-  background-color: white;
-  height: 76px;
+  background-color: #fff;
+  height: 102px;
   top: 0;
   left: 0;
   right: 0;
   position: fixed;
-  display: flex;
   padding: 0 16px;
-  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.14), 0px 3px 4px rgba(0, 0, 0, 0.12),
-    0px 1px 5px rgba(0, 0, 0, 0.2);
+  display: flex;
   align-items: center;
 `
 
 export const TopMobileMenu = () => (
   <Header>
-    <Link to="/">
-      <LogoImage src={LOGO} alt="logo" />
+    <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+      <img src={LOGO_FR} alt="" style={{ height: 70, marginRight: 16 }} />
+      <img src={LOGO} alt="logo" style={{ height: 48 }} />
     </Link>
-    <div className="flex-1" />
-    <Text>Proposé par</Text>
-    <a href="https://www.actionlogement.fr/" target="_blank" rel="noreferrer">
-      <LogoImagePartener src={LOGO_AL} alt="Action logement" />
-    </a>
-    <a href="https://www.pole-emploi.fr/" target="_blank" rel="noreferrer">
-      <LogoImagePartener src={LOGO_PE} alt="Pôle Emploi" />
-    </a>
   </Header>
 )
