786415d2dfa099d1c518f589a7d270c7e21da745
Hugo Agbonon <hugo@codeheroics.com>
2022-02-08 12:20:58 +0100

    docker: entirely remove unused front-admin section

--- front-admin/src/api/auth.api.js
+++
@@ -1,7 +0,0 @@
-export const login = (email, password) => new Promise((resolve) => {
-  resolve({ id: 1, email, password })
-})
-
-export const currentUser = () => new Promise((resolve) => {
-  resolve({ id: 1, email: 'fxaviermontigny@gmail.com' })
-})
--- front-admin/src/components/menu.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-export const Menu = () => (
-  <div>
-    <Link to="/">Home</Link>
-    <Link to="/login">Login</Link>
-  </div>
-)
