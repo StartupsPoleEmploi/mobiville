f01d8aa0e41f5defc4ef60e06eb58c7a8b1f903e
Hugo Agbonon <hugo@codeheroics.com>
2022-02-14 15:55:28 +0100

    api: remove entirely useless seeders files

--- api/src/db/seeders/production/202103080933-clean-descriptions.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  up: async (queryInterface, Sequelize, models) => {
-    await models.cities.update({ description: null }, { where: {} })
-  },
-  down: (/*queryInterface , Sequelize*/) => {},
-}
