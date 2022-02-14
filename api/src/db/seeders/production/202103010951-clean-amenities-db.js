aaa790a5631fd7b927701ba9d011526f010cfb1f
Hugo Agbonon <hugo@codeheroics.com>
2022-02-14 15:55:28 +0100

    api: remove entirely useless seeders files

--- api/src/db/seeders/production/202103010951-clean-amenities-db.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  up: async (queryInterface, Sequelize, models) => {
-    await models.amenities.destroy({
-      where: {},
-      force: true,
-    })
-  },
-  down: async (/*queryInterface /*, Sequelize*/) => {},
-}
