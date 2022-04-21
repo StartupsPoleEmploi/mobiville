b463be3b7e9d9a121a7673d19362db2a24d6fe74
GEFFRY Florian <florian.geffry-ext@pole-emploi.fr>
2022-04-21 17:36:05 +0200

    fix lint

--- front/src/utils/utils.js
+++ front/src/utils/utils.js
@@ -1,5 +1,5 @@
 import moment from 'moment'
-import {useEffect, useRef, useState} from "react";
+import { useEffect, useRef, useState } from 'react'
 
 moment.locale('fr', {
   months:
@@ -86,7 +86,7 @@ export const formatNumber = (number) =>
 
 export const useElementOnScreen = (options) => {
   const containerRef = useRef()
-  const [ isVisible, setIsVisible ] = useState(false)
+  const [isVisible, setIsVisible] = useState(false)
 
   const callbackFunction = (entries) => {
     const [entry] = entries
@@ -98,7 +98,7 @@ export const useElementOnScreen = (options) => {
     if (containerRef.current) observer.observe(containerRef.current)
 
     return () => {
-      if(containerRef.current) observer.unobserve(containerRef.current)
+      if (containerRef.current) observer.unobserve(containerRef.current)
     }
   }, [containerRef, options])
 
