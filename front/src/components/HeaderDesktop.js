53347d29d4d50ddf7cd0547682817475da7ffaf4
Hamza BENHAMIDOU <EP09701@actionlogement.fr>
2022-08-26 14:28:33 +0200

    Supprimer moteur de recherche dans la HP

--- front/src/components/HeaderDesktop.js
+++ front/src/components/HeaderDesktop.js
@@ -1,44 +1,60 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import styled from 'styled-components'
 import { Typography } from '@mui/material'
 
-import { COLOR_HEADER_BACKGROUND, COLOR_TEXT_SECONDARY, COLOR_WHITE } from '../constants/colors'
+import {
+  COLOR_HEADER_BACKGROUND,
+  COLOR_PRIMARY,
+  COLOR_TEXT_SECONDARY,
+} from '../constants/colors'
 import LOGO from '../assets/images/LogoMobiville_gros.svg'
 import LOGO_FR from '../assets/images/marianne-logo.png'
 import LOGO_AL from '../assets/images/logo-action-logement.png'
 import LOGO_PE from '../assets/images/logo-pole-emploi.png'
 
 const Header = styled.header`
-  height: 100px;
-  width: 100%;
+  height: 102px;
+  top: 0;
+  left: 0;
+  right: 0;
   z-index: 1001; //leaflet map est a 1000zindex
-  padding: 12px 80px;
-
+  background: #ffffff;
   display: flex;
   align-items: center;
-  justify-content: space-between;
-
-  background: ${COLOR_WHITE};
+  justify-content: space-around;
+  padding: 16px 70px;
   border-bottom: solid 2px ${COLOR_HEADER_BACKGROUND};
+
+  .taille-fixe {
+    flex-shrink: 0;
+    width: 300px;
+  }
 `
 
-const HomeLink = styled(Link)`
-  height: 100%;
+const Item = styled(Link)`
+  && {
+    margin: 0px 11px;
+    text-decoration: none;
+    position: relative;
+    display: flex;
+    align-items: center;
+    color: ${(props) =>
+      props.selected ? COLOR_PRIMARY : COLOR_TEXT_SECONDARY};
+  }
+`
 
+const IconsContainer = styled.div`
   display: flex;
-  align-items: center;
-  gap: 12px;
+  justify-content: center;
+  flex-grow: 1;
 
-  & > img {
-    height: 100%;
+  a:hover {
+    color: ${COLOR_PRIMARY};
   }
 `
 
 const Text = styled(Typography)`
-  color: ${COLOR_TEXT_SECONDARY};
-  padding-bottom: 8px;
-
   && {
     font-weight: 500;
     font-size: 14px;
@@ -46,61 +62,79 @@ const Text = styled(Typography)`
   }
 `
 
-const PartnersContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-
-const PartnersLogoContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-`
-
 const LogoImagePartner = styled.img`
   height: 28px;
+  margin-left: 16px;
 `
 
-const HeaderDesktop = () => {
-    return (
-      <Header>
-        <HomeLink
-            to="/"
-            title="Retour à l’accueil"
-        >
-          <img
-              src={LOGO_FR}
-              alt="Retour à la page d’accueil"
-          />
-          <img
-              src={LOGO}
-              alt="Retour à la page d’accueil"
-          />
-        </HomeLink>
+const HeaderDesktop = (props) => {
+  const location = useLocation()
 
-        <PartnersContainer>
-          <Text>Proposé par</Text>
-          <PartnersLogoContainer>
-            <a
-                href="https://www.actionlogement.fr/"
-                target="_blank"
-                rel="noreferrer"
-            >
-              <LogoImagePartner src={LOGO_AL} alt="Action logement"/>
-            </a>
-            <a
-                href="https://www.pole-emploi.fr/"
-                target="_blank"
-                rel="noreferrer"
-            >
-              <LogoImagePartner src={LOGO_PE} alt="Pôle Emploi"/>
-            </a>
-          </PartnersLogoContainer>
-        </PartnersContainer>
-      </Header>
-    )
+  const searchSelected = location.pathname.includes('rechercher')
+  const helpSelected = location.pathname.includes('aides')
+
+  function displaySearch() {
+    const flag = props.displaySearch
+    if (flag)
+      return (
+        <IconsContainer>
+          <Item to="/rechercher" selected={searchSelected}>
+            <Text>Rechercher une ville</Text>
+          </Item>
+          <Item to="/aides" selected={helpSelected}>
+            <Text>Rechercher des aides</Text>
+          </Item>
+        </IconsContainer>
+      )
+  }
+
+  return (
+    <Header>
+      <Link
+        to="/"
+        className="taille-fixe"
+        title="Retour à l’accueil"
+        style={{ display: 'flex', alignItems: 'center' }}
+      >
+        <img
+          src={LOGO_FR}
+          alt="Retour à la page d’accueil"
+          style={{ height: 70, marginRight: 16 }}
+        />
+        <img
+          src={LOGO}
+          alt="Retour à la page d’accueil"
+          style={{ height: 85 }}
+        />
+      </Link>
+      {displaySearch()}
+      <div className="taille-fixe" style={{ display: 'flex' }}>
+        <div style={{ marginLeft: 'auto' }}>
+          <Text style={{ color: COLOR_TEXT_SECONDARY, paddingBottom: 8 }}>
+            Proposé par
+          </Text>
+          <a
+            href="https://www.actionlogement.fr/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <LogoImagePartner src={LOGO_AL} alt="Action logement" />
+          </a>
+          <a
+            href="https://www.pole-emploi.fr/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <LogoImagePartner src={LOGO_PE} alt="Pôle Emploi" />
+          </a>
+        </div>
+      </div>
+    </Header>
+  )
 }
 
 HeaderDesktop.propTypes = {}
+
+HeaderDesktop.defaultProps = {}
 
 export default HeaderDesktop
