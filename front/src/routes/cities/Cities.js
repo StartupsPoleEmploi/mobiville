355d3c97742d38a5f969f26cebc19b4fce539084
ISCO2920 <simon.coirie@pole-emploi.fr>
2022-09-08 19:12:52 +0200

    Correctif Liste villes mobile

--- front/src/routes/cities/Cities.js
+++ front/src/routes/cities/Cities.js
@@ -46,7 +46,7 @@ const DesktopContainer = styled.div`
 
 const CitiesList = styled.div`
   max-width: ${({ isMobile }) => (isMobile ? 'auto' : '620px')};
-  padding: ${({ isMobile }) => (isMobile ? '0 16px' : '0 8px 0 8px')};
+  padding: ${({ isMobile }) => (isMobile ? '10 16px' : '0 8px 0 8px')};
 
   a {
     margin: 8px -4px;
