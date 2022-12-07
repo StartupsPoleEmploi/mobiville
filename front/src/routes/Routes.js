b5f6a4d7a281cf40d62d61373a36f277b010200c
GEFFRY Florian <florian.geffry-ext@pole-emploi.fr>
2022-12-07 17:19:00 +0100

    fix merge

--- front/src/routes/Routes.js
+++ front/src/routes/Routes.js
@@ -1,9 +1,6 @@
-import React, { lazy, Suspense } from 'react'
-import {
-  Navigate, Route,
-  Routes, BrowserRouter
-} from 'react-router-dom'
-//import { useNomPage } from '../common/contexts/NomPageContext'
+import React, { lazy, useEffect } from 'react'
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
+import { useNomPage } from '../common/contexts/NomPageContext'
 
 const Home = lazy(() => import('./home/Home'))
 
@@ -13,7 +10,7 @@ const CitySearchPage = lazy(() => import('./city/CitySearchPage'))
 
 const Helps = lazy(() => import('./helps/Helps'))
 const HelpDetailsPage = lazy(() => import('./helps/HelpDetailsPage'))
-//const HelpFilterMobilePage = lazy(() => import('./helps/HelpsFilterMobilePage'))
+const HelpFilterMobilePage = lazy(() => import('./helps/HelpsFilterMobilePage'))
 const HelpsSearchPage = lazy(() => import('./helps/HelpsSearchPage'))
 
 const MobilityGuide = lazy(() => import('./mobility-guide/MobilityGuide'))
@@ -21,37 +18,41 @@ const FAQ = lazy(() => import('./faq/Faq'))
 const Legal = lazy(() => import('./legal/Legal'))
 const Accessibility = lazy(() => import('./accessibility/Accessibility'))
 
-export const MobivilleRoutes = () => (
-  <Suspense fallback={<p>Chargement...</p>}>
-    <BrowserRouter>
-      <Routes>
-        <Route end path="/" element={<Home />} />
+export const MobivilleRoutes = () => {
+  const { setNomPage } = useNomPage()
+  let location = useLocation()
+  useEffect(() => {
+    setNomPage(location.pathname.replace('/', ''))
+  }, [location])
 
-        <Route end path="/villes" element={<Cities />} />
-        <Route end path="/ville/:insee" element={<City />} />
-        <Route end path="/ville/:insee/:section" element={<City />} />
-        <Route end path="/rechercher" element={<CitySearchPage />} />
+  return (
+    <Routes>
+      <Route end path="/" element={<Home />} />
 
-        <Route end path="/aides" element={<Helps />} />
-        <Route end path="/aides/:slug" element={<HelpDetailsPage />} />
-        <Route end path="/aides-filtres" element={<HelpsSearchPage />} />
-        <Route end path="/aides-recherche" element={<HelpsSearchPage />} />
+      <Route end path="/villes" element={<Cities />} />
+      <Route end path="/ville/:insee" element={<City />} />
+      <Route end path="/ville/:insee/:section" element={<City />} />
+      <Route end path="/rechercher" element={<CitySearchPage />} />
 
-        <Route end path="/conseils-et-astuces" element={<MobilityGuide />} />
-        <Route end path="/faq" element={<FAQ />} />
-        <Route end path="/mentions-legales" element={<Legal />} />
-        <Route end path="/accessibilite" element={<Accessibility />} />
+      <Route end path="/aides" element={<Helps />} />
+      <Route end path="/aides/:slug" element={<HelpDetailsPage />} />
+      <Route end path="/aides-filtres" element={<HelpFilterMobilePage />} />
+      <Route end path="/aides-recherche" element={<HelpsSearchPage />} />
 
-        {/* Redirection sur la home page si le path ne match aucune route */}
-        <Route
-          path="*"
-          element={
-            <>
-              <Navigate to="/" replace />
-            </>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  </Suspense>
-)
+      <Route end path="/conseils-et-astuces" element={<MobilityGuide />} />
+      <Route end path="/faq" element={<FAQ />} />
+      <Route end path="/mentions-legales" element={<Legal />} />
+      <Route end path="/accessibilite" element={<Accessibility />} />
+
+      {/* Redirection sur la home page si le path ne match aucune route */}
+      <Route
+        path="*"
+        element={
+          <>
+            <Navigate to="/" replace />
+          </>
+        }
+      />
+    </Routes>
+  )
+}
