5e19d51a0419b263f15232681b8e01ade8a1b141
Alexis Aigueparse <alexis.aigueparse-ext@pole-emploi.fr>
2022-12-14 14:07:29 +0100

    fix: modale height iOS

--- front/src/components/Modale.js
+++ front/src/components/Modale.js
@@ -9,12 +9,10 @@ const Container = styled.div`
   position: fixed;
   top: 0;
   left: 0;
+  right: 0;
+  bottom: 0;
   z-index: 1000;
-  width: 100vw;
-  height: 100vh;
-  height: -moz-available;
-  height: -webkit-fill-available;
-  height: fill-available;
+
   padding: 20px;
 
   overflow-y: scroll;
