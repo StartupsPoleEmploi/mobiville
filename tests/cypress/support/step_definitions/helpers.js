d3f400fc1b1606f286812b430728d05c6347fbf1
GEFFRY Florian <florian.geffry-ext@pole-emploi.fr>
2023-03-20 17:31:12 +0100

    maj popup cookies

--- tests/cypress/support/step_definitions/helpers.js
+++ tests/cypress/support/step_definitions/helpers.js
@@ -1,7 +1,7 @@
 const { Given, When, Then, And } = require('@badeball/cypress-cucumber-preprocessor');
 import { SHORT_WAIT_TIME, MIDDLE_WAIT_TIME, LONG_WAIT_TIME } from "./common/common";
 
-const boutonToutAccepter = "button[type=button][id=footer_tc_privacy_button_2]";
+const boutonToutAccepter = "button[type=button][id=footer_tc_privacy_button]";
 
 Given("Je/je suis sur (l'accueil) mobiville/Mobiville", () => {
   cy.visit('/', {timeout: MIDDLE_WAIT_TIME});
